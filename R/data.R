7d3bdc18a73dfcfcc3a494a3d9fa311d439b6ad9
Davidebag <davide.bagordo01@universitadipavia.it>
2024-05-03 10:49:07 +0000

    adding licenc

--- R/data.R
+++ R/data.R
@@ -8,8 +8,8 @@
 #' @format ##
 #' A tibble with 33 columns:
 #' \describe{
-#'  \item{variable}(IL6)
-#'  \item{description}(measurement of IL6 levels in blood in pg/ml)
+#'  \item{variable}{IL6}
+#'  \item{description}{measurement of IL6 levels in blood in pg/ml}
 #' }
 #'
 #' @source data generated with the simstudy package
